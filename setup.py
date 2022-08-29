bca6292bb5fb36911ad1a1ebf742a17d8b187833
Chris <>
2022-08-29 19:22:59 +0100

    add wheel

---
+++ setup.py
@@ -0,0 +1,30 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='fbmarketplace_model',
+    version='0.0.1',
+    python_requires=">=3.8",
+    packages=find_packages(
+        where='fbRecommendation',
+        include=['fbRecommendation.dl*'],
+        exclude=['fbRecommendation.dl.pytorch'],
+    ),
+    install_requires=[
+        "numpy",
+        "pandas",
+        "spacy",
+        "tqdm",
+        "pillow",
+        "scikit-learn",
+        "tensorflow",
+        "tensorflow-text",
+        "tensorflow_addons",
+        "tf-models-official",
+        "tensorboard",
+        "gensim",
+        "sklearn",
+        "matplotlib",
+        "gin-config",
+        "emoji"
+    ],
+)
